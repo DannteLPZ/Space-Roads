284bdabc7e6d4f0f18d2f5229cf2c1e288f0c404
JohanF29 <82334887+JohanF29@users.noreply.github.com>
2024-03-21 15:09:16 -0500

    Envio

--- Space Roads/Assets/Scripts/Player/DestroyProjectile.cs
+++ Space Roads/Assets/Scripts/Player/DestroyProjectile.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class DestroyProjectile : MonoBehaviour
 {
+    private GameObject playerController;
+    private int upperBound = 6;
+    private float pointsScore = 5.0f;
+    //GameObject.Find("Capsule").GetComponent<MoveUp>().speed = 500;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("Triangle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y > 6)
+        if(transform.position.y > upperBound)
         {
             Destroy(gameObject);
+            
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
        Destroy(gameObject);
+       Destroy(collision.gameObject);
+       playerController.GetComponent<PlayerController>().score += pointsScore;
     }
 }
